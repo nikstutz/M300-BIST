f6e60ca9e257fa9b9f0d2a109e3020f773a76dbb
nikstutz <78543563+nikstutz@users.noreply.github.com>
2021-10-06 15:27:04 +0200

    Add files via upload

---
+++ 30-Container/NEW-VM/Scripts/vm-setup.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt install docker-ce           # das sind 360 MB
+sudo apt-get install docker-compose -y
+sudo git clone https://github.com/nikstutz/M300-BIST.git
+cd M300-BIST/30-Container/Docker-files/
+#sudo docker run -t -p 8011:80 
+#sudo docker-compose up -d
